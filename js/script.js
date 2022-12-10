ceb522601b6a74f3a549f34834700b98a58a1570
EsterSPR <web.esterspreafico@gmail.com>
2022-12-10 18:29:50 +0100

    completato bonus esercizio precedente

--- js/script.js
+++ js/script.js
@@ -1,20 +1,100 @@
 document.getElementById('play-game').addEventListener('click', function (){
-    generateGameGrid();
+    // generateGameGrid();
+    createNewGame();
 })
 
 
+// let arrayBombs = [];
+// arrayBombs = createBombsArray(1, cellsNumber);
+// console.log(arrayBombs);
+
 //FUNZIONI
 
-function generateGameGrid() {
-    document.querySelector('.grid').innerHTML = '';
-    for(let i = 0; i < 100; i++){
-        const cell = document.createElement('div');
-        cell.classList.add('square');
-        cell.innerText =i+1;
+// function generateGameGrid() {
+//     document.querySelector('.grid').innerHTML = '';
+//     for(let i = 0; i < 100; i++){
+//         const cell = document.createElement('div');
+//         cell.classList.add('square');
+//         cell.innerText =i+1;
+//         cell.addEventListener('click', function(){
+//             this.classList.toggle('clicked');
+//             console.log(this.innerText);
+//         });
+//         document.querySelector('.grid').appendChild(cell);
+//     }
+// }
+
+// function createBombsArray(min, max) {
+//     let bombs = [];
+//     let i = 0;
+//     while (i < 16){
+//         let number = Math.floor(Math.random() * (max - min + 1)) + min;
+
+//         if (!bombs.includes(number)){
+//             bombs.push(number);
+//             i++;
+//         }
+
+//         return bombs;
+//     }
+// }
+
+function createNewGame(){
+    let difficulty = parseInt(document.getElementById('level').value);
+
+    let cellsNumber;
+    let cellsPerRow;
+
+    switch(difficulty){
+        case 1:
+            cellsNumber = 100;
+            cellsPerRow = 10;
+            break;
+        case 2:
+            cellsNumber = 81;
+            cellsPerRow = 9;
+            break;
+        case 3:
+            cellsNumber = 49;
+            cellsPerRow = 7;
+            break;
+        default:
+            cellsNumber = 100;
+            cellsPerRow = 10;
+            break;
+    }
+
+    generateGameGrid(cellsNumber, cellsPerRow);
+}
+
+function createSingleCell(num, cells_per_row){
+    const cell = document.createElement('div');
+
+    cell.classList.add('square');
+
+    let sideLength = `calc(100% / ${cells_per_row})`;
+
+    cell.style.width = sideLength;
+    cell.style.height = sideLength;
+    cell.innerText = num;
+
+    return cell;
+}
+
+function generateGameGrid(cellsNumber, cellsPerRow){
+    document.querySelector('.container').innerHTML = '';
+    
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+
+    for (let i = 0; i < cellsNumber; i++){
+        const cell = createSingleCell(i+1, cellsPerRow);
         cell.addEventListener('click', function(){
             this.classList.toggle('clicked');
-            console.log(this.innerText);
-        });
-        document.querySelector('.grid').appendChild(cell);
+        })
+
+        grid.appendChild(cell);
     }
+
+    document.querySelector('.container').appendChild(grid);
 }
--- readme.txt
+++ readme.txt
@@ -1,5 +1,7 @@
-PROBLEMA: Generare una griglia di gioco quadrata al clic dell'utente su un bottone.
+PROBLEMA: Aggiungere logica di gioco alla griglia
 
-1 - Creare un bottone
-2 - Al clic creare una griglia numerata 10 x 10
-3 - Al clic sulla casella questa si colora di azzurro ed emette un messaggio in console col numero della cella cliccata
+1 - Aggiungere le 16 bombe in un array
+    IF l'utente clicca la bomba, la cella diventa rossa e il gioco si ferma
+    ELSE la cella si colora di azzurro e continua il gioco
+2 - La partita finisce se l'utente perde o se clicca tutti i numeri che non sono bombe
+3 - Al termine della partita il software comunica all'utente il suo punteggio, cioè il numero di caselle non-bomba cliccate
